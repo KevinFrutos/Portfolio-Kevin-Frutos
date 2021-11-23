6278cdc4ce3b53246d9e7e9db4d05bbc75706efa
KevinFrutos <vinke.14297@gmail.com>
2021-11-23 22:35:00 +0100

    Experiencia, titulos animados

--- scripts/experiencia.js
+++ scripts/experiencia.js
@@ -9,3 +9,35 @@ home.onmouseleave = () => {
     home.textContent = "Kevin."
     home.classList.remove("animacion")
 }
+
+// ANIMACIONES
+const puestos = ["DISEÑADOR WEB", "PRACTICAS AVELLANA DIGITAL"]
+const puesto = document.querySelectorAll(".puesto")
+const letraTemplate = document.querySelector("#letraTemplate")
+
+for (let i = 0; i < puestos.length; i++) {
+    let puesto_trabajo = puestos[i]
+    console.log(puesto_trabajo);
+    for (let j = 0; j < puesto_trabajo.length; j++) {
+        const clone = letraTemplate.content.cloneNode(true)
+        if(puesto_trabajo.charAt(j) == " "){
+            clone.appendChild(document.createTextNode( '\u00A0' ))
+        }else{
+            clone.querySelector(".letra").textContent = puesto_trabajo.charAt(j)
+        }
+        console.log(puesto_trabajo.charAt(i));
+        puesto[i].appendChild(clone)
+    }
+}
+
+const letra = document.querySelectorAll(".letra")
+
+letra.forEach(item => {
+    item.onmouseover = () => {
+        item.classList.add("letra-deformada")
+    }
+
+    item.onanimationend = () => {
+        item.classList.remove("letra-deformada")
+    }
+})
