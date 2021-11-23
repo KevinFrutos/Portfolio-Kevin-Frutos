b32dfa28a8c5623c16545a1d7cba8f724ede5df8
KevinFrutos <vinke.14297@gmail.com>
2021-11-24 00:41:52 +0100

    Fade-in Up en el Responsive de telefono

--- scripts/main.js
+++ scripts/main.js
@@ -1,136 +1,84 @@
+const html = document.querySelector("html")
 const experiencia = document.querySelector("#experiencia")
 const estudios = document.querySelector("#estudios")
 const about = document.querySelector("#about")
-let ancho = window.innerWidth
+const ancho = window.innerWidth
 
-window.onresize = () => {
-    ancho = window.innerWidth
 
-    if (ancho > 576) {
-        experiencia.onmouseover = () => {
-            experiencia.textContent = "Experiencia"
-            experiencia.classList.add("animacion")
-        }
-
-        experiencia.onmouseleave = () => {
-            experiencia.textContent = "Hola!"
-            experiencia.classList.remove("animacion")
-        }
-
-        estudios.onmouseover = () => {
-            estudios.textContent = "Estudios"
-            estudios.classList.add("animacion")
-        }
-
-        estudios.onmouseleave = () => {
-            estudios.textContent = "Me llamo"
-            estudios.classList.remove("animacion")
-        }
-
-        about.onmouseover = () => {
-            about.textContent = "¿Quien soy?"
-            about.classList.add("animacion")
-        }
-
-        about.onmouseleave = () => {
-            about.textContent = "Kevin."
-            about.classList.remove("animacion")
-        }
-        
-    } else {
-
-        experiencia.onclick = () => {
-            experiencia.textContent = "Experiencia"
-            experiencia.classList.add("animacion")
-        }
-
-        experiencia.onmouseleave = () => {
-            experiencia.textContent = "Hola!"
-            experiencia.classList.remove("animacion")
-        }
-
-        estudios.onclick = () => {
-            estudios.textContent = "Estudios"
-            estudios.classList.add("animacion")
-        }
-
-        estudios.onmouseleave = () => {
-            estudios.textContent = "Me llamo"
-            estudios.classList.remove("animacion")
-        }
-
-        about.onclick = () => {
-            about.textContent = "¿Quien soy?"
-            about.classList.add("animacion")
-        }
-
-        about.onmouseleave = () => {
-            about.textContent = "Kevin."
-            about.classList.remove("animacion")
-        }
-    }
+experiencia.onmouseover = () => {
+    experiencia.textContent = "Experiencia"
+    experiencia.classList.add("animacion")
 }
 
-if (ancho > 576) {
-    experiencia.onmouseover = () => {
-        experiencia.textContent = "Experiencia"
-        experiencia.classList.add("animacion")
-    }
+experiencia.onmouseleave = () => {
+    experiencia.textContent = "Hola!"
+    experiencia.classList.remove("animacion")
+}
 
-    experiencia.onmouseleave = () => {
-        experiencia.textContent = "Hola!"
-        experiencia.classList.remove("animacion")
-    }
+estudios.onmouseover = () => {
+    estudios.textContent = "Estudios"
+    estudios.classList.add("animacion")
+}
 
-    estudios.onmouseover = () => {
-        estudios.textContent = "Estudios"
-        estudios.classList.add("animacion")
-    }
+estudios.onmouseleave = () => {
+    estudios.textContent = "Me llamo"
+    estudios.classList.remove("animacion")
+}
 
-    estudios.onmouseleave = () => {
-        estudios.textContent = "Me llamo"
-        estudios.classList.remove("animacion")
-    }
+about.onmouseover = () => {
+    about.textContent = "¿Quien soy?"
+    about.classList.add("animacion")
+}
 
-    about.onmouseover = () => {
-        about.textContent = "¿Quien soy?"
-        about.classList.add("animacion")
-    }
+about.onmouseleave = () => {
+    about.textContent = "Kevin."
+    about.classList.remove("animacion")
+}
 
-    about.onmouseleave = () => {
-        about.textContent = "Kevin."
-        about.classList.remove("animacion")
-    }
-    
-} else {
+let toggle = false
 
-    experiencia.onclick = () => {
-        experiencia.textContent = "Experiencia"
-        experiencia.classList.add("animacion")
-    }
+if (ancho <= 576) {
+    html.onclick = () => {
+        if (!toggle) {
+            setTimeout(() => {
+                experiencia.textContent = "Experiencia"
+                experiencia.classList.add("fade-in")
+            }, 100);
+            setTimeout(() => {
+                estudios.textContent = "Estudios"
+                estudios.classList.add("fade-in")
+            }, 200);
+            setTimeout(() => {
+                about.textContent = "¿Quien soy?"
+                about.classList.add("fade-in")
+            }, 400);
 
-    experiencia.onmouseleave = () => {
-        experiencia.textContent = "Hola!"
-        experiencia.classList.remove("animacion")
-    }
+            experiencia.classList.remove("fade-in")
+            estudios.classList.remove("fade-in")
+            about.classList.remove("fade-in")
 
-    estudios.onclick = () => {
-        estudios.textContent = "Estudios"
-        estudios.classList.add("animacion")
-    }
+            toggle = true
+        } else {
+            setTimeout(() => {
+                experiencia.textContent = "Hola!"
+                experiencia.classList.add("fade-in")
+            }, 100);
+            setTimeout(() => {
+                estudios.textContent = "Me llamo"
+                estudios.classList.add("fade-in")
+            }, 200);
+            setTimeout(() => {
+                about.textContent = "Kevin."
+                about.classList.add("fade-in")
+            }, 400);
 
-    estudios.onmouseleave = () => {
-        estudios.textContent = "Me llamo"
-        estudios.classList.remove("animacion")
-    }
+            experiencia.classList.remove("fade-in")
+            estudios.classList.remove("fade-in")
+            about.classList.remove("fade-in")
 
-    about.onclick = () => {
-        about.textContent = "¿Quien soy?"
-        about.classList.add("animacion")
-    }
+            toggle = false
+        }
 
-    about.onmouseleave = () => {
-        about.textContent = "Kevin."
-        about.classList.remove("animacion")
+
     }
 }
