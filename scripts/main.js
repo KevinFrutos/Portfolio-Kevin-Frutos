df77d90df08a0468ef046d69385e691f3db89142
KevinFrutos <vinke.14297@gmail.com>
2021-11-24 00:04:13 +0100

    Arreglando fallitos

--- scripts/main.js
+++ scripts/main.js
@@ -70,3 +70,67 @@ window.onresize = () => {
         }
     }
 }
+
+if (ancho > 576) {
+    experiencia.onmouseover = () => {
+        experiencia.textContent = "Experiencia"
+        experiencia.classList.add("animacion")
+    }
+
+    experiencia.onmouseleave = () => {
+        experiencia.textContent = "Hola!"
+        experiencia.classList.remove("animacion")
+    }
+
+    estudios.onmouseover = () => {
+        estudios.textContent = "Estudios"
+        estudios.classList.add("animacion")
+    }
+
+    estudios.onmouseleave = () => {
+        estudios.textContent = "Me llamo"
+        estudios.classList.remove("animacion")
+    }
+
+    about.onmouseover = () => {
+        about.textContent = "¿Quien soy?"
+        about.classList.add("animacion")
+    }
+
+    about.onmouseleave = () => {
+        about.textContent = "Kevin."
+        about.classList.remove("animacion")
+    }
+    
+} else {
+
+    experiencia.onclick = () => {
+        experiencia.textContent = "Experiencia"
+        experiencia.classList.add("animacion")
+    }
+
+    experiencia.onmouseleave = () => {
+        experiencia.textContent = "Hola!"
+        experiencia.classList.remove("animacion")
+    }
+
+    estudios.onclick = () => {
+        estudios.textContent = "Estudios"
+        estudios.classList.add("animacion")
+    }
+
+    estudios.onmouseleave = () => {
+        estudios.textContent = "Me llamo"
+        estudios.classList.remove("animacion")
+    }
+
+    about.onclick = () => {
+        about.textContent = "¿Quien soy?"
+        about.classList.add("animacion")
+    }
+
+    about.onmouseleave = () => {
+        about.textContent = "Kevin."
+        about.classList.remove("animacion")
+    }
+}
