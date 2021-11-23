bb79303f1a744edb7c64e12164a12580ae37f4f1
KevinFrutos <vinke.14297@gmail.com>
2021-11-23 23:57:06 +0100

    Intentando arreclar un link que en dispositivos moviles no funciona bien

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,75 @@
 const experiencia = document.querySelector("#experiencia")
 const estudios = document.querySelector("#estudios")
 const about = document.querySelector("#about")
+let ancho = window.innerWidth
 
-experiencia.onmouseover = () => {
-    experiencia.textContent = "Experiencia"
-    experiencia.classList.add("animacion")
-}
+window.onresize = () => {
+    ancho = window.innerWidth
 
-experiencia.onmouseleave = () => {
-    experiencia.textContent = "Hola!"
-    experiencia.classList.remove("animacion")
-}
 
-estudios.onmouseover = () => {
-    estudios.textContent = "Estudios"
-    estudios.classList.add("animacion")
-}
+    console.log(ancho);
 
-estudios.onmouseleave = () => {
-    estudios.textContent = "Me llamo"
-    estudios.classList.remove("animacion")
-}
+    if (ancho > 576) {
+        experiencia.onmouseover = () => {
+            experiencia.textContent = "Experiencia"
+            experiencia.classList.add("animacion")
+        }
 
-about.onmouseover = () => {
-    about.textContent = "¿Quien soy?"
-    about.classList.add("animacion")
-}
+        experiencia.onmouseleave = () => {
+            experiencia.textContent = "Hola!"
+            experiencia.classList.remove("animacion")
+        }
 
-about.onmouseleave = () => {
-    about.textContent = "Kevin."
-    about.classList.remove("animacion")
+        estudios.onmouseover = () => {
+            estudios.textContent = "Estudios"
+            estudios.classList.add("animacion")
+        }
+
+        estudios.onmouseleave = () => {
+            estudios.textContent = "Me llamo"
+            estudios.classList.remove("animacion")
+        }
+
+        about.onmouseover = () => {
+            about.textContent = "¿Quien soy?"
+            about.classList.add("animacion")
+        }
+
+        about.onmouseleave = () => {
+            about.textContent = "Kevin."
+            about.classList.remove("animacion")
+        }
+        
+    } else {
+
+        experiencia.onclick = () => {
+            experiencia.textContent = "Experiencia"
+            experiencia.classList.add("animacion")
+        }
+
+        experiencia.onmouseleave = () => {
+            experiencia.textContent = "Hola!"
+            experiencia.classList.remove("animacion")
+        }
+
+        estudios.onclick = () => {
+            estudios.textContent = "Estudios"
+            estudios.classList.add("animacion")
+        }
+
+        estudios.onmouseleave = () => {
+            estudios.textContent = "Me llamo"
+            estudios.classList.remove("animacion")
+        }
+
+        about.onclick = () => {
+            about.textContent = "¿Quien soy?"
+            about.classList.add("animacion")
+        }
+
+        about.onmouseleave = () => {
+            about.textContent = "Kevin."
+            about.classList.remove("animacion")
+        }
+    }
 }
