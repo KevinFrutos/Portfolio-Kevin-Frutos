e420aa47de204920f5da669e96049bfba07bb7d8
KevinFrutos <vinke.14297@gmail.com>
2021-11-23 22:42:06 +0100

    Estudios con titulos animados

--- scripts/estudios.js
+++ scripts/estudios.js
@@ -9,3 +9,37 @@ home.onmouseleave = () => {
     home.textContent = "Kevin."
     home.classList.remove("animacion")
 }
+
+
+
+// ANIMACIONES
+const titulos = ["CFGS: DESARROLLO DE APLICACIONES WEB", "CFGM: SISTEMAS MICROINFORMÁTICOS Y REDES", "ESO: EDUCACIÓN SECUNDARIA OBLIGATORIA"]
+const titulo = document.querySelectorAll(".titulo")
+const letraTemplate = document.querySelector("#letraTemplate")
+
+for (let i = 0; i < titulos.length; i++) {
+    let titulo_estudios = titulos[i]
+    console.log(titulo_estudios);
+    for (let j = 0; j < titulo_estudios.length; j++) {
+        const clone = letraTemplate.content.cloneNode(true)
+        if(titulo_estudios.charAt(j) == " "){
+            clone.appendChild(document.createTextNode( '\u00A0' ))
+        }else{
+            clone.querySelector(".letra").textContent = titulo_estudios.charAt(j)
+        }
+        console.log(titulo_estudios.charAt(i));
+        titulo[i].appendChild(clone)
+    }
+}
+
+const letra = document.querySelectorAll(".letra")
+
+letra.forEach(item => {
+    item.onmouseover = () => {
+        item.classList.add("letra-deformada")
+    }
+
+    item.onanimationend = () => {
+        item.classList.remove("letra-deformada")
+    }
+})
