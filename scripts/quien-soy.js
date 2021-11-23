da0b64a4ef2ab0b6b2e1f58a1f84be67483c5f2a
KevinFrutos <vinke.14297@gmail.com>
2021-11-23 22:47:01 +0100

    Titulos de las tres paginas animados

--- scripts/quien-soy.js
+++ scripts/quien-soy.js
@@ -9,3 +9,36 @@ home.onmouseleave = () => {
     home.textContent = "Kevin."
     home.classList.remove("animacion")
 }
+
+
+// ANIMACIONES
+const nombres = ["KEVIN VALENTIN FRUTOS RIVERA"]
+const nombre = document.querySelectorAll(".nombre")
+const letraTemplate = document.querySelector("#letraTemplate")
+
+for (let i = 0; i < nombres.length; i++) {
+    let name = nombres[i]
+    console.log(name);
+    for (let j = 0; j < name.length; j++) {
+        const clone = letraTemplate.content.cloneNode(true)
+        if(name.charAt(j) == " "){
+            clone.appendChild(document.createTextNode( '\u00A0' ))
+        }else{
+            clone.querySelector(".letra").textContent = name.charAt(j)
+        }
+        console.log(name.charAt(i));
+        nombre[i].appendChild(clone)
+    }
+}
+
+const letra = document.querySelectorAll(".letra")
+
+letra.forEach(item => {
+    item.onmouseover = () => {
+        item.classList.add("letra-deformada")
+    }
+
+    item.onanimationend = () => {
+        item.classList.remove("letra-deformada")
+    }
+})
